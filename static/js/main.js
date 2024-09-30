fadab2dd3d9fef816adf11a32320c930b8ec721e
DamirPages <123557281+DamirPages@users.noreply.github.com>
2024-10-01 03:36:58 +0500

    Add files via upload

--- static/js/main.js
+++ static/js/main.js
@@ -105,6 +105,18 @@ if (reviewsSlider) {
     }
   });
 }
+var reviewsButtons = document.querySelectorAll('.reviews__item');
+reviewsButtons.forEach(function (item) {
+  item.addEventListener('click', function (e) {
+    var imageUrl = item.dataset.reviewImg;
+    if (window.innerWidth < 992) {
+      new Fancybox([{
+        src: imageUrl,
+        type: "image"
+      }]);
+    }
+  });
+});
 "use strict";
 
 function _createForOfIteratorHelper(r, e) { var t = "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (!t) { if (Array.isArray(r) || (t = _unsupportedIterableToArray(r)) || e && r && "number" == typeof r.length) { t && (r = t); var _n = 0, F = function F() {}; return { s: F, n: function n() { return _n >= r.length ? { done: !0 } : { done: !1, value: r[_n++] }; }, e: function e(r) { throw r; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var o, a = !0, u = !1; return { s: function s() { t = t.call(r); }, n: function n() { var r = t.next(); return a = r.done, r; }, e: function e(r) { u = !0, o = r; }, f: function f() { try { a || null == t.return || t.return(); } finally { if (u) throw o; } } }; }
@@ -260,4 +272,15 @@ document.addEventListener('DOMContentLoaded', function () {
   } finally {
     _iterator.f();
   }
+});
+var fileInputs = document.querySelectorAll('.file-input');
+fileInputs.forEach(function (item) {
+  item.addEventListener('change', function (e) {
+    console.log(e);
+    if (e.target.files[0] != 'undefined') {
+      item.querySelector('.file-input-name').textContent = e.target.files[0].name;
+    } else {
+      item.querySelector('.file-input-name').textContent = '';
+    }
+  });
 });
